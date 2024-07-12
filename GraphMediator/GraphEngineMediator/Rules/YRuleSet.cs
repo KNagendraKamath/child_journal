4af81af30e4cf0d4ea1e088d2297b6237c6bf0e9
Fred George <fredgeorge@acm.org>
2024-07-12 08:12:54 +0530

    Fred: More formatting

--- GraphMediator/GraphEngineMediator/Rules/YRuleSet.cs
+++ GraphMediator/GraphEngineMediator/Rules/YRuleSet.cs
@@ -1,23 +1,13 @@
 ﻿using GraphEngine.Graph;
 using static GraphEngine.Graph.AxisFactory;
 
-namespace GraphMediator.GraphEngineMediator
-{
-    internal class YRuleSet(string label) : RuleSet
-    {
-      
-        public AxisFactory Factory(int recordCount)
-        {
-            return new AxisFactory([new SingleTestRule(label)]);
-        }
-    }
-    
-    // TODO: This class is not used in the codebase. Should it be removed?
-    internal class SingleTestRule(string label) : Rule
-    {
-        public void Check(double min, double max)
-        {
-            throw new AxisException(new Axis(min, max, 1, label));
-        }
-    }
+namespace GraphMediator.GraphEngineMediator;
+
+internal class YRuleSet(string label) : RuleSet {
+    public AxisFactory Factory(int recordCount) => new([new SingleTestRule(label)]);
+}
+
+// TODO: This class is only used above. Should it be collapsed?
+internal class SingleTestRule(string label) : Rule {
+    public void Check(double min, double max) => throw new AxisException(new Axis(min, max, 1, label));
 }
