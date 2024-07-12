c51a6e26e817de18553907670b23884529b1bd03
Fred George <fredgeorge@acm.org>
2024-07-12 13:43:31 +0530

    Fred: Starting to work on Age scalse

--- Engine.Tests/Unit/ScaleTest.cs
+++ Engine.Tests/Unit/ScaleTest.cs
@@ -13,17 +13,22 @@ using static GraphEngine.Quantities.Unit;
 namespace GraphEngine.Tests.Unit {
     public class ScaleTest {
         private static readonly Dimension d_cm = new(
-            "TestDimension",
+            "cm Test",
             Centimeter,
             new List<RatioQuantity> { 1.cm(), 5.cm(), 10.cm(), 20.cm(), 50.cm() });
+        
+        private static readonly Dimension d_age = new(
+            "age Test",
+            Year,
+            new List<RatioQuantity> { 1.Weeks(), 2.Weeks(), 1.Months(), 2.Months(), 5.Months(), 0.5.Years(), 1.Years(), 2.Years(), 5.Years()});
 
         [Fact]
-        public void NoRoundingScale() {
+        public void SimpleMetricsScale() {
             Assert.Equal(
-                new Scale("TestDimension", 50.cm(), 70.cm(), 5.cm()),
+                new Scale("cm Test", 50.cm(), 70.cm(), 5.cm()),
                 d_cm.Axis(50.cm(), 70.cm(), 10));
             Assert.Equal(
-                new Scale("TestDimension", 50.cm(), 70.cm(), 5.cm()),
+                new Scale("cm Test", 50.cm(), 70.cm(), 5.cm()),
                 d_cm.Axis(52.5.cm(), 67.8.cm(), 10));
             ;
         }
