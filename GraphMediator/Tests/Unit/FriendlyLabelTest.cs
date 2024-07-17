c7bcd1abf25f6dc084bac945d94fcede7a10424f
Fred George <fredgeorge@acm.org>
2024-07-17 09:37:18 +0530

    Fred: Formatting sequences of Age correctly

--- GraphMediator/Tests/Unit/FriendlyLabelTest.cs
+++ GraphMediator/Tests/Unit/FriendlyLabelTest.cs
@@ -26,6 +26,14 @@ public class FriendlyLabelTest {
         AssertLabels(["12m"], [12.Months()], AgeFormatter.Instance);
         AssertLabels(["2y"], [2.Years()], AgeFormatter.Instance);
         AssertLabels(["10y6m"], [10.5.Years()], AgeFormatter.Instance);
+        AssertLabels(
+            ["0m", "1m", "2m", "3m", "4m", "5m"], 
+            [0.Months(), 1.Months(), 2.Months(), 3.Months(), 4.Months(), 5.Months()], 
+            AgeFormatter.Instance);
+        AssertLabels(
+            ["9y", "9y3m", "9y6m", "9y9m", "10y", "10y3m", "10y6m"], 
+            [9.Years(), 9.25.Years(), 9.5.Years(), 9.75.Years(), 10.Years(), 10.25.Years(), 10.5.Years()], 
+            AgeFormatter.Instance);
     }
     
     private void AssertLabels(List<string> expected, List<RatioQuantity> quantities, FriendlyFormatter formatter) {
